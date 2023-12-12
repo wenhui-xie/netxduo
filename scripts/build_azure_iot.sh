789a2600666eaab3d45d178920ff38d19d2b9b9f
Wenhui Xie <wenxi@microsoft.com>
2023-12-12 01:56:13 +0000

    Add step for Memory measurement.

--- scripts/build_azure_iot.sh
+++ scripts/build_azure_iot.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
 $(dirname `realpath $0`)/../test/cmake/azure_iot/run.sh build all
+cd $(dirname $0)/../test/cmake/azure_iot/build/default_build_coverage
+for i in `find . -name "*.a"`; do arm-none-eabi-size --totals $i; echo ""; done
